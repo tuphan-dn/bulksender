9d9c150de7c2de0a71eb8c90bac31b5afcc4455e
tuphan-dn <tuphan@descartes.network>
2021-10-14 18:09:17 +0700

    _

--- modulefederation.config.js
+++ modulefederation.config.js
@@ -5,9 +5,9 @@ module.exports = {
     react: { singleton: true, requiredVersion: '^17.0.2' },
     'react-dom': { singleton: true, requiredVersion: '^17.0.2' },
     'react-router-dom': { singleton: true, requiredVersion: '^5.3.0' },
-    antd: { singleton: true, requiredVersion: '^4.17.0-alpha.5' },
     '@reduxjs/toolkit': { singleton: true, requiredVersion: '^1.6.2' },
     'react-redux': { singleton: true, requiredVersion: '^7.2.5' },
+    antd: { singleton: true, requiredVersion: '^4.17.0-alpha.5' },
   },
   remotes: {
     senos: 'senos@http://localhost:5000/index.js',
