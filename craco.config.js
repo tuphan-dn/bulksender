a02d2889017f7302525a2eb81c2ca32ed97009b4
tuphan-dn <tuphan@descartes.network>
2021-10-14 23:23:09 +0700

    _

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,26 @@ const cracoModuleFederation = require('craco-module-federation')
 module.exports = {
   plugins: [{ plugin: cracoModuleFederation }],
   webpack: {
-    plugins: [new NodePolyfillPlugin()], // Fix polyfill in webpack 5
+    plugins: [
+      new NodePolyfillPlugin(), // Support polyfill in webpack 5
+    ],
+    configure: (webpackConfig, { env, paths }) => {
+      // Bugfix: https://github.com/graphql/graphql-js/issues/2721#issuecomment-723008284
+      const {
+        module: { rules, ...rest },
+      } = webpackConfig
+      rules.push({
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      })
+      webpackConfig.module = {
+        rules,
+        ...rest,
+      }
+      return webpackConfig
+    },
   },
   devServer: {
     client: {
