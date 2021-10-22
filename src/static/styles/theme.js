6c14568879ef557023d7ab9a261643bcfd510dc7
tuphan-dn <tuphan@descartes.network>
2021-10-23 01:11:38 +0700

    caching fix

--- src/static/styles/theme.js
+++ src/static/styles/theme.js
@@ -3,7 +3,6 @@
 module.exports = {
   // Colors
   '@primary-color': '#F9575E',
-  // '@primary-color': '#FFFFFF',
   '@info-color': '#37CDFA',
   '@success-color': '#3DBA4E',
   '@warning-color': '#FCB017',
