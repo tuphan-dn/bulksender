49d8fece22e76e70a77d6adc0389fb733f344709
tuphan-dn <tuphan@descartes.network>
2021-10-23 16:29:48 +0700

    _

--- src/app/page/index.tsx
+++ src/app/page/index.tsx
@@ -1,31 +1,10 @@
-import { useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
-import { Row, Col, Typography, Button } from 'antd'
-
-import { AppDispatch, AppState } from 'app/model'
-import { increaseCounter } from 'app/model/main.controller'
-import { useWallet } from 'senhub/providers'
+import { Row, Col, Typography } from 'antd'
 
 const Page = () => {
-  const {
-    wallet: { address },
-  } = useWallet()
-  const dispatch = useDispatch<AppDispatch>()
-  const { counter } = useSelector((state: AppState) => state.main)
-  const increase = useCallback(() => dispatch(increaseCounter()), [dispatch])
-
   return (
     <Row gutter={[24, 24]} align="middle">
       <Col span={24}>
-        <Typography.Title level={5}>Sen Page</Typography.Title>
-        <Typography.Text>Address: {address}</Typography.Text>
-      </Col>
-      <Col>
-        <Typography.Text>Counter: {counter}</Typography.Text>
-      </Col>
-      <Col>
-        <Button onClick={increase}>Increase</Button>
+        <Typography.Title level={1}>Solana Bulk Sender</Typography.Title>
       </Col>
     </Row>
   )
