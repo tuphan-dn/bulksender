8a2c5d4364af70bf18a0ec68f4364d137276cd8e
tuphan-dn <tuphan@descartes.network>
2021-10-23 11:23:31 +0700

    remove package-lock

--- src/view/header/settings/index.tsx
+++ src/view/header/settings/index.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 
 import { Row, Col, Button, Popover, Typography, Divider, Badge } from 'antd'
@@ -12,29 +11,27 @@ import { RootState } from 'store'
 const Settings = () => {
   const { networkStatus } = useSelector((state: RootState) => state.ui)
 
-  const content = useMemo(() => {
-    return (
-      <Row gutter={[16, 16]} style={{ maxWidth: 300 }}>
-        <Col span={24}>
-          <Typography.Title level={5} type="secondary">
-            System Settings
-          </Typography.Title>
-        </Col>
-        <Col span={24}>
-          <Mode />
-        </Col>
-        <Col span={24}>
-          <Network />
-        </Col>
-        <Col span={24}>
-          <Divider style={{ margin: 0 }} />
-        </Col>
-        <Col span={24}>
-          <Wallet />
-        </Col>
-      </Row>
-    )
-  }, [networkStatus])
+  const content = (
+    <Row gutter={[16, 16]} style={{ maxWidth: 300 }}>
+      <Col span={24}>
+        <Typography.Title level={5} type="secondary">
+          System Settings
+        </Typography.Title>
+      </Col>
+      <Col span={24}>
+        <Mode />
+      </Col>
+      <Col span={24}>
+        <Network />
+      </Col>
+      <Col span={24}>
+        <Divider style={{ margin: 0 }} />
+      </Col>
+      <Col span={24}>
+        <Wallet />
+      </Col>
+    </Row>
+  )
 
   return (
     <Popover placement="topRight" trigger="click" content={content}>
