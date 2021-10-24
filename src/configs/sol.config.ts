67131dceaf98883483e4316643808861c2c27dc9
tuphan-dn <tuphan@descartes.network>
2021-10-24 22:42:39 +0700

    account, pool watcher

--- src/configs/sol.config.ts
+++ src/configs/sol.config.ts
@@ -21,36 +21,36 @@ const conf: Record<Net, Conf> = {
    * Development configurations
    */
   devnet: {
+    ...SOLVARS,
     node: 'https://api.devnet.solana.com',
     chainId: 103,
     senAddress: '5YwUkPdXLoujGkZuo9B4LsLKj3hdkDcfP4derpspifSJ',
     swapAddress: '4erFSLP7oBFSVC1t35jdxmbfxEhYCKfoM6XdG2BLR3UF',
     taxmanAddress: '8UaZw2jDhJzv5V53569JbCd3bD4BnyCfBH3sjwgajGS9',
-    ...SOLVARS,
   },
 
   /**
    * Staging configurations
    */
   testnet: {
+    ...SOLVARS,
     node: 'https://api.testnet.solana.com',
     chainId: 102,
     senAddress: '',
     swapAddress: '',
     taxmanAddress: '',
-    ...SOLVARS,
   },
 
   /**
    * Production configurations
    */
   mainnet: {
+    ...SOLVARS,
     node: 'https://api.mainnet-beta.solana.com',
     chainId: 101,
     senAddress: 'SENBBKVCM7homnf5RX9zqpf1GFe935hnbU4uVzY1Y6M',
     swapAddress: 'SSW7ooZ1EbEognq5GosbygA3uWW1Hq1NsFq6TsftCFV',
     taxmanAddress: '9doo2HZQEmh2NgfT3Yx12M89aoBheycYqH1eaR5gKb3e',
-    ...SOLVARS,
   },
 }
 
--- src/view/watcher/index.tsx
+++ src/view/watcher/index.tsx
@@ -1,12 +1,16 @@
 import { Fragment } from 'react'
 import UIWatcher from './ui.watcher'
 import WalletWatcher from './wallet.watcher'
+import AccountWatcher from './account.watcher'
+import PoolWatcher from './pool.watcher'
 
 const Watcher = () => {
   return (
     <Fragment>
       <UIWatcher />
       <WalletWatcher />
+      <AccountWatcher />
+      <PoolWatcher />
     </Fragment>
   )
 }
