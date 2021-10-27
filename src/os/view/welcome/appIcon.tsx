7b3a915a7e04564a1dfe99dc5cefd52e0ddc7f3a
tuphan-dn <tuphan@descartes.network>
2021-10-27 09:55:42 +0700

    app name

--- src/os/view/welcome/appIcon.tsx
+++ src/os/view/welcome/appIcon.tsx
@@ -1,6 +1,9 @@
-import { Row, Col, Card, Typography } from 'antd'
 import { useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
+
+import { Row, Col, Card, Typography } from 'antd'
+
+import register from 'senhub.register'
 
 const AppIcon = ({ appId }: { appId: string }) => {
   const history = useHistory()
@@ -13,7 +16,7 @@ const AppIcon = ({ appId }: { appId: string }) => {
     <Card onClick={launch} hoverable>
       <Row gutter={[24, 24]}>
         <Col>
-          <Typography.Title level={5}>{appId}</Typography.Title>
+          <Typography.Title level={5}>{register[appId].name}</Typography.Title>
         </Col>
       </Row>
     </Card>
