a4c1444c0e07296c2408a48d64c3d55bc01ca719
tuphan-dn <tuphan@descartes.network>
2021-10-28 17:48:24 +0700

    ipfs, silence webpack, action center

--- modulefederation.config.js
+++ modulefederation.config.js
@@ -11,7 +11,6 @@ module.exports = {
     '@reduxjs/toolkit': { singleton: true, requiredVersion: '^1.6.2' },
     'react-redux': { singleton: true, requiredVersion: '^7.2.5' },
     antd: { singleton: true, requiredVersion: '^4.17.0-alpha.7' },
-    'ipfs-core': { singleton: true, requiredVersion: '^0.11.1' },
   },
   remotes: {
     senhub,
