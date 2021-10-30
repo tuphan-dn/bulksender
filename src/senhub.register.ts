8b8724d959e04c6dc00baaa813be226d9379637c
tuphan-dn <tuphan@descartes.network>
2021-10-30 23:35:36 +0700

    fix context menu

--- src/senhub.register.ts
+++ src/senhub.register.ts
@@ -1,27 +1,20 @@
-import { env } from 'shared/runtime'
 import configs from 'app/configs'
 
 const {
   manifest: { url, appId },
 } = configs
 
-const fixed: SenHubRegister = {
-  senstore: {
+const register: SenHubRegister = {
+  [appId]: {
+    url: `${url}/index.js`,
+    appId,
+    name: 'My App',
+  },
+  bulksender: {
     url: 'https://descartesnetwork.github.io/senstore/index.js',
     appId: 'senstore',
     name: 'Sen Store',
   },
 }
 
-const register: SenHubRegister =
-  env === 'production'
-    ? {}
-    : {
-        [appId]: {
-          url: `${url}/index.js`,
-          appId,
-          name: 'My App',
-        },
-      }
-
-export default { ...register, ...fixed }
+export default register
