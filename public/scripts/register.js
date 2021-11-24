f9d0714fa3ddb161e1602f2de6ab597b3d6f6103
dmcong <83624415+dmcong@users.noreply.github.com>
2021-11-24 21:45:46 +0700

    feature script register

---
+++ public/scripts/register.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const {
+  parsed: {
+    REACT_APP_ID,
+    REACT_APP_NAME,
+    REACT_APP_AUTHOR_NAME,
+    REACT_APP_AUTHOR_EMAIL,
+    REACT_APP_TAGS,
+    REACT_APP_URL,
+    REACT_APP_DESCRIPTION,
+  },
+} = require('dotenv-cra').config()
+
+const myApp = {
+  url: `${REACT_APP_URL}/index.js`,
+  appId: REACT_APP_ID,
+  name: REACT_APP_NAME,
+  author: {
+    name: REACT_APP_AUTHOR_NAME || '',
+    email: REACT_APP_AUTHOR_EMAIL || '',
+  },
+  tags: (REACT_APP_TAGS || '').split(','),
+  description: REACT_APP_DESCRIPTION || '',
+  verified: false,
+}
+
+const fileName = `register-${REACT_APP_ID}.json`
+fs.writeFileSync(fileName, JSON.stringify(myApp, null, 2))
+
+console.log(
+  '\x1b[33m',
+  '\n👏👏👏 Finished build register file. Check it out-->',
+  '\x1b[36m',
+  fileName,
+  '\x1b[0m\n',
+)
