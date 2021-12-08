1d0494df0a08a959791624ea82d68b53b5da7c60
tuphan-dn <tuphan@descartes.network>
2021-12-08 09:16:30 +0700

    support wasm, web3

--- plugins/craco-compatibility.js
+++ plugins/craco-compatibility.js
@@ -12,10 +12,11 @@ const overrideWebpackConfig = ({ context, webpackConfig, pluginOptions }) => {
       Buffer: ['buffer', 'Buffer'],
     }),
   )
-  // Add util polyfill
-  // (For IPFS/Web3 compatibility)
+  // Add polyfill libraries
+  // (For IPFS/WASM/Web3 compatibility)
   webpackConfig.resolve.fallback = {
     util: require.resolve('util/'),
+    stream: require.resolve('stream-browserify'),
     assert: require.resolve('assert/'),
   }
   // Fix unrecognized change / caching problem
