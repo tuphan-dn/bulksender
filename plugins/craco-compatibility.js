dc44d0c088c8a50172ed50b46fe1d81541e0f779
tuphan-dn <tuphan@descartes.network>
2021-12-08 12:05:47 +0700

    clarify plugins

--- plugins/craco-compatibility.js
+++ plugins/craco-compatibility.js
@@ -1,11 +1,11 @@
 /**
- * Maintain webpack 5 compatibility
+ * Maintain Webpack 5 compatibility
  */
 const path = require('path')
 const webpack = require('webpack')
 
 const overrideWebpackConfig = ({ context, webpackConfig, pluginOptions }) => {
-  // Add buffer to webpack 5 polyfill
+  // Add buffer to Webpack 5 polyfill
   // https://github.com/diegomura/react-pdf/issues/1029
   webpackConfig.plugins.push(
     new webpack.ProvidePlugin({
@@ -13,17 +13,16 @@ const overrideWebpackConfig = ({ context, webpackConfig, pluginOptions }) => {
     }),
   )
   // Add polyfill libraries
-  // (For IPFS/WASM/Web3 compatibility)
   webpackConfig.resolve.fallback = {
-    util: require.resolve('util/'),
-    stream: require.resolve('stream-browserify'),
-    assert: require.resolve('assert/'),
+    util: require.resolve('util/'), // For IPFS
+    stream: require.resolve('stream-browserify'), // For WASM
+    assert: require.resolve('assert/'), // For Ethereum Web3
   }
   // Fix unrecognized change / caching problem
   webpackConfig.cache.buildDependencies.config.push(
     path.join(context.paths.appPath, './craco.config.js'),
   )
-  // Fix fully specified
+  // Fix "fully specified"
   // https://github.com/webpack/webpack/issues/11467#issuecomment-808618999/
   webpackConfig.module.rules.push({
     test: /\.m?js/,
