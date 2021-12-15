87ecc360d265bcfdce5c652328049aa685a7338a
tuphan-dn <tuphan@descartes.network>
2021-12-15 15:34:27 +0700

    fix navigation

--- src/os/view/header/contextMenu/index.tsx
+++ src/os/view/header/contextMenu/index.tsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux'
 import { useLocation } from 'react-router-dom'
 import { account } from '@senswap/sen-js'
 
-import { Row, Col, Divider } from 'antd'
+import { Row, Col } from 'antd'
 import Navigation from './navigation'
 import Search from './search'
 
@@ -10,20 +10,14 @@ import { RootState } from 'os/store'
 
 const ContextMenu = () => {
   const { pathname } = useLocation()
-  const { width } = useSelector((state: RootState) => state.ui)
   const { address } = useSelector((state: RootState) => state.wallet)
 
+  if (pathname.startsWith('/store')) return <Search />
   if (!account.isAddress(address)) return null
   return (
-    <Row gutter={[12, 12]} wrap={false} align="middle">
-      <Col>
-        <Divider
-          type="vertical"
-          style={{ margin: `0px ${width < 768 ? 4 : 8}px` }}
-        />
-      </Col>
-      <Col flex="auto" style={{ width: 0 }} className="scrollbar">
-        {pathname.startsWith('/store') ? <Search /> : <Navigation />}
+    <Row gutter={[12, 12]}>
+      <Col span={24} style={{ width: 0, marginLeft: 8 }} className="scrollbar">
+        <Navigation />
       </Col>
     </Row>
   )
