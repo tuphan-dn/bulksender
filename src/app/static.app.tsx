b5ff02a560d971d33b892bc0b64e41c7b26a1a43
tuphan-dn <tuphan@descartes.network>
2021-12-16 17:06:02 +0700

    panels

--- src/app/static.app.tsx
+++ src/app/static.app.tsx
@@ -1,5 +1,6 @@
 import panel from 'app/static/images/panel.png'
+import preview from 'app/static/images/preview.png'
 
-export const panels = [panel]
+export const panels = [panel, preview]
 export { default as logo } from 'app/static/images/logo.png'
 export { default as readme } from 'app/static/docs/README.md'
