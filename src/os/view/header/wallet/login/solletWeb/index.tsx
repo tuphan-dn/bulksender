2321d818889c1f215462fd74bbb228731236f320
tuphan-dn <tuphan@descartes.network>
2021-12-27 15:49:30 +0700

    rename Sollet

--- src/os/view/header/wallet/login/solletWeb/index.tsx
+++ src/os/view/header/wallet/login/solletWeb/index.tsx
@@ -25,7 +25,7 @@ const SolletWeb = () => {
       style={{ cursor: 'pointer' }}
       bordered={false}
       hoverable
-      bodyStyle={{ padding: 16 }}
+      bodyStyle={{ padding: '24px 16px' }}
     >
       <Row gutter={[16, 16]} justify="center">
         <Col>
