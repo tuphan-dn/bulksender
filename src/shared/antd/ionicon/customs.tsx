d72c047c09f33a24fd9a5ae407795cd380b7e3c1
tuanle161099 <tuanle@descartes.network>
2022-01-07 11:47:47 +0700

    fix ui, add alert icon

--- src/shared/antd/ionicon/customs.tsx
+++ src/shared/antd/ionicon/customs.tsx
@@ -1,5 +1,7 @@
 import IconTele from './assets/icon-telegram.svg'
+import IconAlert from './assets/icon-alert.svg'
 
 export const CUSTOM_ICONS: Record<string, string> = {
   'logo-telegram': IconTele,
+  'icon-alert': IconAlert,
 }
