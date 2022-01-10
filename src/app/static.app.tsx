0bca6aaae36cf5bdab926e3a91544c7a0d89d076
tuphan-dn <tuphan@descartes.network>
2022-01-10 14:58:58 +0700

    _

--- src/app/static.app.tsx
+++ src/app/static.app.tsx
@@ -1,8 +1,6 @@
 import panel from 'app/static/images/panel.png'
 import preview from 'app/static/images/preview.png'
 
-console.log('run test')
-
 export const panels = [panel, preview]
 export { default as logo } from 'app/static/images/logo.png'
 export { default as readme } from 'app/static/docs/README.md'
