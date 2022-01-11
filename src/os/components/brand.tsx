f6d52068aaef33c54b68e672c8bb1a917b10ff07
thiennv <thiennguyen@descartes.network>
2022-01-11 14:53:48 +0700

    update network brand

--- src/os/components/brand.tsx
+++ src/os/components/brand.tsx
@@ -1,9 +1,29 @@
-import { CSSProperties } from 'react'
+import { CSSProperties, useMemo } from 'react'
 import { useHistory } from 'react-router-dom'
+
+import { net } from 'shared/runtime'
 
 import fullLogo from 'os/static/images/sen-full.png'
 import fullLogoDark from 'os/static/images/sen-full-dark.png'
 import liteLogo from 'os/static/images/sen-lite.png'
+import fullLogoDev from 'os/static/images/sen-dev.svg'
+import fullLogoDevDark from 'os/static/images/sen-dev-dark.svg'
+import fullLogoTest from 'os/static/images/sen-test.svg'
+import fullLogoTestDark from 'os/static/images/sen-test-dark.svg'
+import liteLogoTest from 'os/static/images/sen-test-lite.svg'
+import liteLogoDev from 'os/static/images/sen-dev-lite.svg'
+
+const LOGO = {
+  mainnet: fullLogo,
+  mainnet_dark: fullLogoDark,
+  mainnet_lite: liteLogo,
+  devnet: fullLogoDev,
+  devnet_dark: fullLogoDevDark,
+  testnet: fullLogoTest,
+  testnet_dark: fullLogoTestDark,
+  devnet_lite: liteLogoDev,
+  testnet_lite: liteLogoTest,
+}
 
 const Brand = ({
   style,
@@ -16,9 +36,15 @@ const Brand = ({
 }) => {
   const history = useHistory()
 
+  const networkLogo = useMemo(() => {
+    if (lite) return LOGO?.[`${net}_lite`]
+    if (darkTheme) return LOGO?.[`${net}_dark`]
+    return LOGO?.[net]
+  }, [darkTheme, lite])
+
   return (
     <img
-      src={lite ? liteLogo : darkTheme ? fullLogoDark : fullLogo}
+      src={networkLogo}
       style={style}
       alt="logo"
       onClick={() => history.push('/')}
