857237b1bd5178c051592e0ffd259605ae1aeb30
thiennv <thiennguyen@descartes.network>
2022-01-11 16:33:36 +0700

    update logo svg

--- src/os/components/brand.tsx
+++ src/os/components/brand.tsx
@@ -3,9 +3,9 @@ import { useHistory } from 'react-router-dom'
 
 import { Net } from 'shared/runtime'
 
-import fullLogo from 'os/static/images/sen-full.png'
-import fullLogoDark from 'os/static/images/sen-full-dark.png'
-import liteLogo from 'os/static/images/sen-lite.png'
+import fullLogo from 'os/static/images/sen-full.svg'
+import fullLogoDark from 'os/static/images/sen-full-dark.svg'
+import liteLogo from 'os/static/images/sen-lite.svg'
 import fullLogoDev from 'os/static/images/sen-dev.svg'
 import fullLogoDevDark from 'os/static/images/sen-dev-dark.svg'
 import fullLogoTest from 'os/static/images/sen-test.svg'
@@ -43,6 +43,7 @@ const Brand = ({
     if (darkTheme) return LOGO[`${network}_dark`]
     return LOGO[network]
   }, [darkTheme, lite, network])
+  console.log(network, networkLogo, 'logo')
 
   return (
     <img
