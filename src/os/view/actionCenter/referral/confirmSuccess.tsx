d3099cd974b201e77656c39e248e4dbad1a38dbb
tuphan-dn <tuphan@descartes.network>
2022-01-20 18:01:34 +0700

    fix typo

--- src/os/view/actionCenter/referral/confirmSuccess.tsx
+++ src/os/view/actionCenter/referral/confirmSuccess.tsx
@@ -5,6 +5,7 @@ import { Button, Col, Image, Modal, Row, Space, Typography } from 'antd'
 import iconSuccessFully from 'os/static/images/icon-cs-referral.svg'
 import { setVisibleActionCenter } from 'os/store/ui.reducer'
 import { RootDispatch, useRootDispatch } from 'os/store'
+import IonIcon from 'shared/antd/ionicon'
 
 const ConfirmSuccessFully = ({
   visible = false,
@@ -28,19 +29,23 @@ const ConfirmSuccessFully = ({
       onCancel={() => onCancel(false)}
       footer={false}
       centered
-      closable={false}
+      closeIcon={<IonIcon name="close" />}
     >
       <Row gutter={[34, 34]} justify="center">
         <Col>
-          <Image src={iconSuccessFully} preview={false} />
+          <Image
+            style={{ marginTop: 32 }}
+            src={iconSuccessFully}
+            preview={false}
+          />
         </Col>
         <Col span={20}>
           <Space size={4} direction="vertical" style={{ textAlign: 'center' }}>
             <Typography.Title level={3}>Successfully!</Typography.Title>
             <Typography.Text type="secondary">
-              You have completed the referral confirmation. Let's deposit to
-              Sentre pools to get{' '}
-              <strong style={{ color: '#F9575E' }}>1 SNTR</strong>
+              You have completed the referral confirmation. Let's complete to
+              the first of mission to get{' '}
+              <strong style={{ color: '#F9575E' }}>10 SNTR</strong>.
             </Typography.Text>
           </Space>
         </Col>
