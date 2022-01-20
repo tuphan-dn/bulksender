17396f05804101bb0588d24a8db12a2d8771fd94
thiennv <thiennguyen@descartes.network>
2022-01-20 15:47:03 +0700

    update guide referral

--- src/os/view/actionCenter/referral/guideReferral.tsx
+++ src/os/view/actionCenter/referral/guideReferral.tsx
@@ -1,26 +1,60 @@
-import { Col, Collapse, Row, Typography } from 'antd'
-import { REFERRAL_CONTENT } from './docReferral'
+import { Col, Row, Typography, Steps } from 'antd'
+import { useState } from 'react'
+import IonIcon from 'shared/antd/ionicon'
 
 const GuideReferral = () => {
+  const [guideStep, setGuideStep] = useState(0)
   return (
     <Row gutter={[8, 8]}>
       <Col span={24}>
         <Typography.Title level={5}>Referral guide</Typography.Title>
       </Col>
       <Col span={24}>
-        <Collapse className="referral-guide-collapse" bordered={false}>
-          {REFERRAL_CONTENT.map((guide, idx) => (
-            <Collapse.Panel
-              header={`Step ${idx + 1}: ${guide.title}`}
-              key={idx}
-              showArrow={false}
-            >
+        <Steps
+          size="small"
+          current={guideStep}
+          onChange={setGuideStep}
+          direction="vertical"
+        >
+          <Steps.Step
+            title={'Connect wallet to join the Sentre'}
+            description={
               <Typography.Text type="secondary">
-                {guide.content}
+                Launch <Typography.Text>hub.sentre.io</Typography.Text> and then
+                select a wallet to connect.
               </Typography.Text>
-            </Collapse.Panel>
-          ))}
-        </Collapse>
+            }
+            key={1}
+          />
+          <Steps.Step
+            title={'Enter referral link'}
+            description={
+              <Typography.Text type="secondary">
+                Click the icon{' '}
+                <Typography.Text>
+                  <IonIcon name="menu-outline" />
+                </Typography.Text>{' '}
+                in the upper right corner {'>'}{' '}
+                <Typography.Text>User</Typography.Text> {'>'}{' '}
+                <Typography.Text>
+                  Enter referral link or upload QR code
+                </Typography.Text>{' '}
+                of referrer {'>'} <Typography.Text>Confirm</Typography.Text>
+              </Typography.Text>
+            }
+            key={2}
+          />
+          <Steps.Step
+            title={'Deposit to Sentre pools'}
+            description={
+              <Typography.Text type="secondary">
+                Deposit from <Typography.Text>1000 SNTR</Typography.Text> to
+                Sentre pools in <Typography.Text>7 days</Typography.Text>
+              </Typography.Text>
+            }
+            key={3}
+          />
+        </Steps>
       </Col>
     </Row>
   )
