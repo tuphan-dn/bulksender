a0ca1361d3def8f199c4dfb43025a3447154456f
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-05-25 15:41:37 +0700

    alter PDB to localStorage for saving selected tokens

--- src/shared/antd/mint/mintSelection/useRecommendedMint.ts
+++ src/shared/antd/mint/mintSelection/useRecommendedMint.ts
@@ -1,31 +1,20 @@
 import { useCallback, useEffect, useState } from 'react'
-import { useWallet } from '@senhub/providers'
 
 import { useAllMintAddresses } from './useAllMintAddresses'
 import { useSortMints } from 'shared/hooks/useSortMints'
 import { net } from 'shared/runtime'
-import { createPDB } from 'shared/pdb'
-import configs from 'app/configs'
-
-const {
-  manifest: { appId },
-} = configs
+import localStorage from 'shared/storage'
 
 const LIMIT_ITEM = 8
-const PDB_ID = `${net}:selected_mints`
+const LOCAL_STORAGE_ID = `${net}:selected_mints`
 
 export const useRecommendedMint = () => {
   const [recommendedMints, setRecommendedMints] = useState<string[]>([])
-  const {
-    wallet: { address },
-  } = useWallet()
   const allMintAddresses = useAllMintAddresses()
   const { sortedMints } = useSortMints(allMintAddresses)
 
   const getRecommendedMints = useCallback(async () => {
-    let mints: string[] = []
-    const pdb = createPDB(address, appId)
-    if (pdb) mints = (await pdb.getItem(PDB_ID)) || []
+    let mints: string[] = localStorage.get(LOCAL_STORAGE_ID) || []
 
     for (const mint of sortedMints) {
       if (mints.length >= LIMIT_ITEM) break
@@ -33,17 +22,17 @@ export const useRecommendedMint = () => {
       mints.push(mint)
     }
     return setRecommendedMints(mints.slice(0, LIMIT_ITEM))
-  }, [address, sortedMints])
+  }, [sortedMints])
 
   const addRecommendMint = useCallback(
     async (mintAddress: string) => {
       const mints = recommendedMints.filter((mint) => mint !== mintAddress)
       const newMints = [mintAddress, ...mints].slice(0, LIMIT_ITEM)
-      const pdb = createPDB(address, appId)
-      if (pdb) await pdb.setItem(PDB_ID, newMints)
+      localStorage.set(LOCAL_STORAGE_ID, newMints)
+
       return setRecommendedMints(newMints)
     },
-    [address, recommendedMints],
+    [recommendedMints],
   )
 
   useEffect(() => {
